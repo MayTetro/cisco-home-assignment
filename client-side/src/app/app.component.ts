5c523cb855ae8588c505a756017dc7e369269cd7
fdsfdsf <gdfgsg>
2021-01-08 15:34:19 +0200

    small fixes

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -9,14 +9,18 @@ import {ArticleService} from './services/article.service';
 })
 export class AppComponent {
   articles: Article[];
+  noArticlesToShow: boolean;
   searchedValue: string;
 
   constructor(private articleService: ArticleService) {
+    this.noArticlesToShow = true;
   }
 
   save(subRedditName: string) {
     this.searchedValue = subRedditName;
     this.articleService.getHeroes(subRedditName).subscribe((articles: Article[]) => {
+      debugger;
+      this.noArticlesToShow = !!articles.length;
       this.articles = articles;
     });
   }
